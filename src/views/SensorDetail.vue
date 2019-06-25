aecb8fc1d311ad033bfc5fe482ecc3e62e3f5bd9
Renato <renatoaure@outlook.com>
2019-06-25 14:07:11 -0300

    Added google maps for the sensor details

--- src/views/SensorDetail.vue
+++ src/views/SensorDetail.vue
@@ -15,11 +15,24 @@
         <lineChart :data="data" :labels="labels" :unit="sensor.unit"/>
       </v-flex>
     </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <GmapMap
+          :center="{lat:sensor.location.y, lng:sensor.location.x}"
+          :zoom="17"
+          map-type-id="satellite"
+          style="width: 100%; height: 300px"
+        >
+          <GmapMarker ref="myMarker" :position="google && new google.maps.LatLng(sensor.location.y, sensor.location.x)" title="asd"/>
+        </GmapMap>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 import lineChart from "../components/charts/lineChart";
+import { gmapApi } from "vue2-google-maps";
 export default {
   data() {
     return {
@@ -55,35 +68,37 @@ export default {
       .catch(error => {
         console.log(error.message);
       })
-      .finally(()=>{
+      .finally(() => {
         this.axios
-      .get(
-        "http://localhost:8888/getSensorInfo?name=" +
-          this.$options.propsData.name
-      )
-      .then(result => {
-        console.log("Resultado axio 2:");
-        console.log(result);
+          .get(
+            "http://localhost:8888/getSensorInfo?name=" +
+              this.$options.propsData.name
+          )
+          .then(result => {
+            console.log("Resultado axio 2:");
+            console.log(result);
 
-        var sensor = result.data.result;
-        var date = new Date(sensor.register);
-        var formatDate =
-          date.getDate() +
-          "/" +
-          (date.getMonth() + 1) +
-          "/" +
-          date.getFullYear();
-        sensor.register = formatDate;
-        this.sensor = sensor;
+            var sensor = result.data.result;
+            var date = new Date(sensor.register);
+            var formatDate =
+              date.getDate() +
+              "/" +
+              (date.getMonth() + 1) +
+              "/" +
+              date.getFullYear();
+            sensor.register = formatDate;
+            this.sensor = sensor;
 
-        console.log(this.sensor);
+            console.log(this.sensor);
+          });
       });
-      });
-    
   },
   beforeUpdate() {},
   components: {
     lineChart
+  },
+  computed: {
+    google: gmapApi
   }
 };
 </script>
