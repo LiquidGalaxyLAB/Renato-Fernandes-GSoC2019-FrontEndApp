fe95cc6a9925ec275f6758c036e6da1398584a55
Renato <renatoaure@outlook.com>
2019-08-21 06:14:06 -0300

    Logos update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,53 +10,55 @@
         </h3>
         <br />
         <v-flex xs12>
-          <v-layout row wrap>
-            <v-flex xs6>
-              <v-img :src="require('../assets/inno4agro-logo.jpg')" />
-            </v-flex>
-            <v-flex xs6>
+          <v-layout row wrap align-center justify-space-around fill-height>
+            <v-flex xs4>
               <v-img :src="require('../assets/liquidsensors.png')" />
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12>
+          <v-layout row wrap align-center justify-space-around fill-height>
+            <v-flex xs4>
+              <v-img :src="require('../assets/LOGO LIQUID GALAXY.jpg')" />
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12>
+          <v-layout row wrap align-center justify-space-around fill-height>
+            <v-flex xs4>
+              <v-img :src="require('../assets/lglab.png')" />
+            </v-flex>
+            <v-flex xs4>
+              <v-img :src="require('../assets/LOGO LABORATORIS TIC.png')" />
+            </v-flex>
+            <v-flex xs4>
+              <v-img :src="require('../assets/pcital logo.jpg')" />
+            </v-flex>
+            <v-flex xs4>
+              <v-img :src="require('../assets/gsoc.png')" />
+            </v-flex>
+            <v-flex xs4>
+              <v-img :src="require('../assets/LogoLGEU.png')" />
             </v-flex>
           </v-layout>
         </v-flex>
         <br />
         <br />
-        <v-divider></v-divider>
-      </v-flex>
-      <v-flex xs12 align-center justify-space-around row fill-height>
-        <br />
-        <br />
-        <h2 class="text-xs-center display-2 font-weight-bold">Collaborators</h2>
       </v-flex>
       <v-flex xs12>
         <v-layout align-center justify-space-around row wrap fill-height>
-          <v-flex xs2>
-            <v-img :src="require('../assets/gsoc.png')" />
+          <v-flex xs3>
+            <v-img :src="require('../assets/inno4agro-logo.jpg')" />
           </v-flex>
-          <v-flex xs2>
-            <v-img :src="require('../assets/LOGO LABORATORIS TIC.png')" />
+          <v-flex xs3>
+            <v-img :src="require('../assets/agro.jpg')" />
           </v-flex>
           <v-flex xs3>
             <v-img :src="require('../assets/LogoFACENS-1210x642.png')" />
           </v-flex>
         </v-layout>
-        <v-layout align-center justify-space-around row wrap fill-height>
-          <v-flex xs3>
-            <v-img :src="require('../assets/lglab.png')" />
-          </v-flex>
-          <v-flex xs3>
-            <v-img :src="require('../assets/LOGO LIQUID GALAXY.jpg')" />
-          </v-flex>
-        </v-layout>
         <br />
-        <v-layout align-center justify-space-around row wrap fill-height>
-          <v-flex xs3>
-            <v-img :src="require('../assets/LogoLGEU.png')" />
-          </v-flex>
-          <v-flex xs3>
-            <v-img :src="require('../assets/pcital logo.jpg')" />
-          </v-flex>
-        </v-layout>
+        <v-layout align-center justify-space-around row wrap fill-height></v-layout>
       </v-flex>
     </v-layout>
   </v-container>
